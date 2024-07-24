bd9d6059a3158b8f38405b9df46f2d0b722ec466
Vishesh jhadi <vishesh@gandhineru.com>
2024-07-24 12:22:21 +0530

    added video bg for server test -- testing

--- app/(page)/componentPage/page.tsx
+++ app/(page)/componentPage/page.tsx
@@ -1,64 +1,76 @@
+"use client";
+import React from "react";
+import ServiceCards from "@/components/CustomUi/ServiceCard";
 import {
   PrimaryButton,
   SecondaryButton,
-  SocialButton,
 } from "@/components/CustomUi/CustomButton";
-import { ArrowRight, ContactIcon } from "@/components/CustomIcons";
-import HeroBanner from "@/components/CustomUi/HeroBanner";
-import ContactForm from "@/components/CustomUi/ContactForm";
-import DemoBlogPost from "@/utils/DemoBlogPost";
-import BlogCard from "@/components/CustomUi/BlogCard";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { MotionDiv } from "@/components/CustomUi/MotionDiv";
 import { cn } from "@/lib/utils";
-import ShflleCard from "@/components/CustomUi/ShflleCard";
-import { AnimatePresence, motion } from "framer-motion";
-
+import { motion } from "framer-motion";
 const page = () => {
-
- 
-  const variantsEnter = {
-    initial: {
-      top: "100%",
-    },
-    start: {
-      top: "0%",
-      opacity: [1],
-    },
-  };
-
-  const variantsExit = {
-    initial: {
-      top: "0%",
-      opacity: 0,
-    },
-    start: {
-      top: "100%",
-      opacity: 1,
-    },
-  };
   return (
-    <section className="h-screen w-screen grid place-content-center">
-      {/* <div className="h-screen">
-        
-        <motion.div 
-        variants={variantsEnter}
-        initial="initial"
-        animate="start"
-        transition={{duration:1, ease:[0.76, 0, 0.24, 1]}}
-        className="fixed top-0 right-0 left-0 w-screen h-screen pointer-events-none z-[100] bg-primary" />
-        <motion.div 
-        variants={variantsExit}
-        initial="initial"
-        animate="start"
-        transition={{duration:1 , delay:1.2 ,ease:[0.76, 0, 0.24, 1]}}
-        className="fixed top-0 right-0 left-0 w-screen h-screen pointer-events-none z-[100] bg-primary" />
-      </div> */}
+    <main className="my-20">
+      <section className="sectionContainer">
+        <MotionDiv
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className={cn(" h-full sticky grid place-content-center top-[20%]")}
+        >
+          {/* <motion.div
+            className="w-full relative h-[420px] md:h-[350px] lg:h-[420px] bg-cover rounded-[20px] bg-center p-10 lg:p-16 flex flex-col justify-end pb-[50px] lg:pb-[80px]"
+            style={{
+              backgroundImage:
+                "url(https://cms.gandhineru.com/wp-content/uploads/2024/06/Web-Development.png)",
+              scale: 1,
+              top: `calc(-5vh + ${1 * 25}px)`,
+            }}
+          > */}
+            <div className="w-full relative h-[420px] md:h-[350px] lg:h-[420px] bg-cover rounded-[20px] bg-center p-10 lg:p-16 flex flex-col justify-end pb-[50px] lg:pb-[80px]">
+              <video
+                src="https://cms.gandhineru.com/wp-content/uploads/2024/07/a_tap_on_the_wall_flowing_popcorns_out_and_that_popcorns_in_getting_collected_into_the_blow__and_the_dd4b05.mp4"
+                autoPlay
+                loop
+                controls={false}
+                className="w-full h-full object-cover object-right rounded-[20px] absolute top-0 left-0 -z-10"
+              ></video>
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                className="w-full md:w-[65%] lg:w-[50%] flex flex-col gap-6"
+              >
+                <h2 className="text-white">Test</h2>
+                <div>
+                  TEst test Lorem ipsum, dolor sit amet consectetur adipisicing
+                  elit. Sapiente amet quod reiciendis provident! Qui labore ut,
+                  maiores sed, facere nostrum, a sit officiis necessitatibus
+                  reprehenderit tempore laboriosam unde. Iusto, quam.
+                </div>
 
+                <div className="flex gap-12">
+                  <PrimaryButton
+                    className="text-white bg-[#242120] text-[12px] md:text-base"
+                    href={`/services/`}
+                  >
+                    Take it ahead
+                  </PrimaryButton>
 
-<PrimaryButton className="bg-red-500" href="#"> All</PrimaryButton>
-<a href="www.google.com">Google</a>
-    </section>
+                  <SecondaryButton
+                    href="/services"
+                    className="text-base text-white text-[12px] md:text-base"
+                    arrowColor="white"
+                  >
+                    View Projects
+                  </SecondaryButton>
+                </div>
+              </motion.div>
+            </div>
+          {/* </motion.div> */}
+        </MotionDiv>
+      </section>
+    </main>
   );
 };
 
