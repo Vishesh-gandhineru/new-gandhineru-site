784e26bdc43585ece7f0ed8536457c4309a29a47
Gandhi & neru <133116648+Vishesh-gandhineru@users.noreply.github.com>
2026-03-23 14:55:13 +0530

    hide upcoming page

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
         <PageTransition />
         <MainMenu />
         {children}
-        <Upcoming />
+        {/* <Upcoming /> */}
         <MainFooter />
       </body>
     </html>
